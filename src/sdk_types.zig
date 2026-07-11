f9d2c549b73df11eec6d2a838b51c26bb078d1dc
immanuel-254 <njugunaimmanuel1@outlook.com>
2026-07-11 17:51:38 +0300

    I am certified with the Address type implementation

--- src/sdk_types.zig
+++ src/sdk_types.zig
@@ -1,18 +1,59 @@
-//! By convention, root.zig is the root source file when making a package.
 const std = @import("std");
-const Io = std.Io;
+const assert = std.debug.assert;
+const testing = std.testing;
 
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
+const AddressLength: u8 = 32;
+
+pub const Address = struct {
+    // allocator: std.mem.Allocator,
+    bytes: [AddressLength]u8,
+
+    pub fn new() Address {
+        //const bytes: []u8 = try allocator.alloc(u8, AddressLength);
+        const bytes: [AddressLength]u8 = undefined;
+
+        return Address{
+            .bytes = bytes,
+        };
+    }
+    pub fn from_hex(self: *Address, hexStr: []const u8) !void {
+        assert(hexStr[0] == '0');
+        assert(hexStr[1] == 'x');
+
+        if (hexStr.len == 0) {
+            return AddressParserError.EmptyInput;
+        }
+        if (hexStr.len > 66) {
+            return AddressParserError.InputTooLong;
+        }
+
+        _ = std.fmt.hexToBytes(self.bytes[0..], hexStr[2..]) catch |err| {
+            return err;
+        };
+    }
+    pub fn from_bytes(self: *Address, bytes: [AddressLength]u8) void {
+        self.bytes = bytes;
+    }
+    pub fn to_hex(self: *Address) []const u8 {
+        const hexStr = "0x" ++ std.fmt.bytesToHex(&self.bytes, .lower);
+        return hexStr;
+    }
+
+    pub fn to_bytes(self: *Address) [AddressLength]u8 {
+        return self.bytes;
+    }
+};
+
+pub const AddressParserError = error{ EmptyInput, InputTooLong, InvalidHexCharacter };
+
+test "Address" {
+    var address = Address.new();
+    const a = "0x0000000000000000000000000000000000000000000000000000000000000002";
+    try address.from_hex(a);
+    try testing.expect(address.bytes.len == AddressLength);
+    const b = address.to_hex();
+    try testing.expectEqualStrings(b, a);
 }
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+// pub const HexDecodeError = enum([]u8) { EmptyInput = "input hex string must be non-empty", InputTooLong = "input hex string is too long for address", InvalidHexCharacter = "input hex string has wrong character" };
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
